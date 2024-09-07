0eb6e91718b7e3506e4b7ccf384fd5e9a198cef1
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-07 15:55:56 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/Comparing Strings.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/Comparing Strings.js
@@ -1,3 +1,21 @@
 console.log("Yes" == "yes")
 
 console.log('Y' != 'y')
+
+
+// Pick a string. Your string can have any number of characters.
+const my_string = "a";
+
+// Calculate the ASCII value of the first character, i.e. the character at the position 0. 
+const ASCII_value = my_string.charCodeAt(0);
+
+// Let us print
+console.log(ASCII_value);
+
+
+const my_strings = "Udacity";
+
+// Iterate using a Loop
+for (let i = 0; i < my_strings.length; i++) {
+  console.log(my_strings.charCodeAt(i));
+}
