9e2ef5051fad9efed3f36303d40c7c55cbdbbd14
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-07 15:49:28 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/Escaping Characters.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/Escaping Characters.js
@@ -1,5 +1,5 @@
 console.log("\"Double Quote\"") // Double Quote
 console.log("\'Single Quote\'") // Single Quote
-console.log("The man whispered, \"please speak to me\".") // Double Quote
-console.log("The man whispered, \"please speak to me\".") // Double Quote
+console.log("New \n Line") // New Line
+console.log("Tab \t Text") // Double Quote
 console.log("The man whispered, \"please speak to me\".") // Double Quote
