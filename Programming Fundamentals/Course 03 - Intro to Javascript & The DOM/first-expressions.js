9bdf30b31c5589d8b854b4052b3db0ebb1439cdf
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-07 14:20:33 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/first-expressions.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/first-expressions.js
@@ -1,7 +1,7 @@
 /*
 In the first-expressions.js file in the code editor below, write an expression that:
-    uses at least 3 different arithmetic operators.
-    equals 42.
+    - uses at least 3 different arithmetic operators.
+    - equals 42.
 
 Hint: +, -, *, /,* and % are possible arithmetic operators*
 */
