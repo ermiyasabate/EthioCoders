f2b44819288fbc8be1a2df1650b45731bf7cd0e1
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-07 17:36:31 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/Quiz/tied-up.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/Quiz/tied-up.js
@@ -6,5 +6,5 @@
  * 2. Your joke should use only one string
  * 3. Your joke should match the expected format
 */
-const joke = /* write your joke here */
+const joke = "Why did the scarecrow win an award? Because he was outstanding in his field!"
 console.log(joke);
