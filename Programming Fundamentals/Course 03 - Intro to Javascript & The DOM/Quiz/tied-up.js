61ebf77c2151fbaa82e91ea97ddd5d48dd38b1b0
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-07 17:35:07 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/Quiz/tied-up.js
@@ -0,0 +1,10 @@
+/*
+ * Programming Quiz: All Tied Up
+ 
+ * Quiz Requirements
+ * 1. Your code should have a variable joke
+ * 2. Your joke should use only one string
+ * 3. Your joke should match the expected format
+*/
+const joke = /* write your joke here */
+console.log(joke);
