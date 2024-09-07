9cad64bc02595378ed01b9bc6a79ba55657cfc25
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-07 16:39:46 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/Quiz/first-expression.js
@@ -0,0 +1,15 @@
+/*
+ * Programming Quiz: First Expression
+ * Write an expression that uses at least three, distinct, arithmetic 
+ * operators to log the number 42 to the console.
+ */
+ 
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should print the value `42`
+ * 2. You should use at least 3 distinct operators.
+ *    (`+`, `-`, `*`, `/`, or `%`) 
+ */
+
+
+// this expression equals 4, change it to equal 42
