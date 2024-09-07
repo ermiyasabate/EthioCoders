27094287ba6f5ada8f57c2de318b5630373c63c2
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-07 14:36:18 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/Variable.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/Variable.js
@@ -4,6 +4,10 @@ var greeting = "Hello";
 console.log(greeting + " World!")
 console.log(greeting + " Mike!")
 
+let dinner = "tacos";
+dinner = dinner + " and " + "tamales";
+console.log(dinner);
+
 /* This is error
     const dinner = "tacos";
     dinner = dinner + " and " + "tamales";
