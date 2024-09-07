3c798d8377f71e8a4a684211df34cb45b51987b7
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-07 17:37:36 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/Quiz/yosa-buson.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: Yosa Buson
+ */
+ 
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should have a variable `haiku`
+ * 2. Your code should use string concatenation
+ * 3. Your poem should match the famous haiku poem
+ */
+
+const haiku = /* concatenate the strings here */
+console.log(haiku);
