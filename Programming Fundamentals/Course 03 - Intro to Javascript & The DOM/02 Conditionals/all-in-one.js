657ed5b3fddef6305a62c00cd98d33cb0d297c2e
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-08 14:19:58 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/02 Conditionals/all-in-one.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/02 Conditionals/all-in-one.js
@@ -8,3 +8,15 @@ if (money >= price) {
 } else {
     console.log("do't buy the hammer")
 }
+
+
+// Else If statement
+const weather = "sunny";
+
+if (weather === "snow") {
+    console.log("Bring a coat.");
+} else if (weather === "rain") {
+    console.log("Bring a rain jacket.");
+} else {
+    console.log("Wear what you have on.");
+}
