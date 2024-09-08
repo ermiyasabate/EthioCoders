4bfd8e7e644baa739713d2a95e891cf48b17fc4c
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-08 14:39:15 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/02 Conditionals/musical-groups.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: Musical Groups
+ */
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should have a variable `musicians`, and include `if...else if...else` conditional statement
+ * 2. Your code should produce the expected output, as mentioned above. Read each condition carefully. 
+ */
+ 
+// change the value of `musicians` to test your conditional statements
+const musicians = 1;
+
+// your code goes here
