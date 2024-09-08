bfb55d28dc4b2b056612a354950312afb8892e3b
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-08 20:34:56 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/02 Conditionals/what-wear.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/02 Conditionals/what-wear.js
@@ -26,3 +26,25 @@ const shirtLength = 30;
 const shirtSleeve = 8.71;
 
 // your code goes here
+const S = 18 <= shirtWidth < 20 && 28 <= shirtLength < 29 && 8.13 <= shirtSleeve < 8.38
+const M = 20 <= shirtWidth < 22 && 29 <= shirtLength < 30 && 8.38 <= shirtSleeve < 8.63
+const L = 22 <= shirtWidth < 24 && 30 <= shirtLength < 31 && 8.63 <= shirtSleeve < 8.88
+const XL = 24 <= shirtWidth < 26 && 31 <= shirtLength < 33 && 8.88 <= shirtSleeve < 9.63
+const Two_XL = 26 <= shirtWidth < 28 && 33 <= shirtLength < 34 && 9.63 <= shirtSleeve < 10.13
+const Three_XL = shirtWidth >= 28 && shirtLength >= 34 && shirtSleeve >= 10.13
+
+if (S){
+    console.log("S")
+} else if (M){
+    console.log("M")
+} else if (L){
+    console.log("L")
+} else if (XL){
+    console.log("XL")
+} else if (Two_XL){
+    console.log("2XL")
+} else if (Three_XL){
+    console.log("3XL")
+} else {
+    console.log("N/A")
+}
