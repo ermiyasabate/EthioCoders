b5b4470d8a3315c48b3c22ca3355c57cfbc57083
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-08 13:35:39 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/Escaping Characters.js
+++
@@ -1,9 +0,0 @@
-console.log("\"Double Quote\"") // Double Quote
-console.log("\'Single Quote\'") // Single Quote
-console.log("New \n Line") // New Line
-console.log("Tab \t Text") // Tab
-console.log("Backslash \\") // Backslash
-
-console.log("Up up\n\tdown down")
-
-console.log("The file located at \"C:\\\Desktop\\My Documents\\Roster\\names.txt\" contains the names on the roster.")
--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/convert.js
+++
@@ -1,5 +0,0 @@
-let celsius = 12
-
-let fahrenheit = (celsius * 1.8) + 32
-
-console.log(celsius + "°C is equals to " + fahrenheit + "°F")
