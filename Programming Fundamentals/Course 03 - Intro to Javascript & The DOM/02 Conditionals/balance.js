f060329d2333e9e168c8e52b81b8e892138030d8
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-08 15:14:38 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/02 Conditionals/balance.js
@@ -0,0 +1,19 @@
+/*
+ * Programming Quiz - Checking Your Balance
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should have the variables `balance`, `checkBalance`, `isActive`
+ * 2. Your code should include an `if...else` conditional statement
+ * 3. Your code should produce the expected output
+ * 4. Your code should not be empty
+ * 5. BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED. 
+ */
+ 
+// change the values of `balance`, `checkBalance`, and `isActive` to test your code
+const balance = 325.00;
+const checkBalance = true;
+const isActive = false;
+
+// your code goes here
