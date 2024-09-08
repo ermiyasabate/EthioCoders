508d466ab41baa7c7e2d416bc59038697c903487
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-08 20:06:35 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/02 Conditionals/what-wear.js
@@ -0,0 +1,28 @@
+/*
+ * Programming Quiz: What do I Wear?
+ *
+ * Using if/else statements, create a series of logical expressions that logs the size of a t-shirt based on the measurements of:
+ *   - shirtWidth
+ *   - shirtLength
+ *   - shirtSleeve
+ *
+ * Use the chart above to print out one of the following correct values:
+ *   - S, M, L, XL, 2XL, or 3XL
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should have the variables `shirtWidth`, `shirtLength`, and `shirtSleeve`
+ * 2. Your code should include an `if...else` conditional statement
+ * 3. Your code should use logical expressions
+ * 4. Your code should produce the expected output.
+ */
+ 
+
+
+// change the values of `shirtWidth`, `shirtLength`, and `shirtSleeve` to test your code
+const shirtWidth = 23;
+const shirtLength = 30;
+const shirtSleeve = 8.71;
+
+// your code goes here
