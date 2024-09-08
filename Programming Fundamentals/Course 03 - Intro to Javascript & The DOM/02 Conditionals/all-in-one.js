0ff97a1cbeb7da11a02da6eb4e2ddcf4422707f4
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-08 14:16:04 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/02 Conditionals/all-in-one.js
@@ -0,0 +1,8 @@
+const price = 15.00; // Price of the hammer
+const money = 20.00; // Our money
+
+if (money >= price) {
+    console.log("buy the hammer")
+} else {
+    console.log("do't buy the hammer")
+}
