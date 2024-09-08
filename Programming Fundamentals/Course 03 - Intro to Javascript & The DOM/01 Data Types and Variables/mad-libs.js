50047b8946e5c429a36cbf2f7408f73b9781bc8b
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-08 14:00:12 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/01 Data Types and Variables/mad-libs.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/01 Data Types and Variables/mad-libs.js
@@ -20,6 +20,6 @@
 let adjective1 = "amazing"
 let adjective2 = "fun"
 let adjective3 = "entertaining"
-let madLib = "The Intro to JavaScript course is " + adjective1 +". James and Julia are so " + adjective2 + ". I cannot wait to work through the rest of this " + adjective3 + " content!"
+let madLib = "The Intro to JavaScript course is " + adjective1 + ". James and Julia are so " + adjective2 + ". I cannot wait to work through the rest of this " + adjective3 + " content!"
 
 console.log(madLib)
