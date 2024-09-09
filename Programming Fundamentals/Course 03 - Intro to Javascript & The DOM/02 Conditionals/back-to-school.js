409036f9d27578a689386573bf2f66ad0be1111f
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-09 13:08:29 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/02 Conditionals/back-to-school.js
@@ -0,0 +1,22 @@
+/*
+ * Programming Quiz: Back to School
+ *
+ * Write a switch statement to set the average salary of a person based on their type of completed education.
+ *
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have the variables `education`, and `salary`
+ * - Your code should include a switch statement
+ * - Your code should produce the expected output
+ */
+ 
+// change the value of `education` to test your code
+const education = "no high school diploma";
+
+// set the value of this based on a person's education
+let salary = 0;
+
+// your code goes here
+console.log('oi')
