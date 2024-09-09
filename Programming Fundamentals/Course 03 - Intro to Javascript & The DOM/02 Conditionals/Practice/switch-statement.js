9aee165de0bb83ae22d2e39f31b9294b2afd5766
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-09 12:51:21 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/02 Conditionals/Practice/switch-statement.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/02 Conditionals/Practice/switch-statement.js
@@ -22,3 +22,28 @@ switch (value) {
     default:
         console.log("You did not select a valid option.");
 }
+
+
+// Example 1 - It doesn't matter where the default statement is placed
+const favoriteFood = "soup";
+let restaurant = undefined;
+
+switch (favoriteFood) {
+    case "pizza":
+        restaurant = "pizzeria";
+        break;
+    default:
+        restaurant = "diner";
+        break;
+    case "tacos":
+        restaurant = "taqueria";
+        break;
+    case "sushi":
+        restaurant = "sushi bar";
+        break;
+    case "pancakes":
+        restaurant = "pancake house";
+        break;
+}
+
+console.log("Go to the " + restaurant);
