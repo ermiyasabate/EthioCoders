b04e19446fc7ebbca96c95bcd700a2594a75edba
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-09 12:47:49 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/02 Conditionals/Practice/switch-statement.js
@@ -0,0 +1,22 @@
+const value = 4
+
+switch (value) {
+    case 1:
+        console.log("You selected option 1.");
+        break;
+    case 2:
+        console.log("You selected option 2.");
+        break;
+    case 3:
+        console.log("You selected option 3.");
+        break;
+    case 4:
+        console.log("You selected option 4.");
+        break;
+    case 5:
+        console.log("You selected option 5.");
+        break;
+    case 6:
+        console.log("You selected option 6.");
+        break;
+}
