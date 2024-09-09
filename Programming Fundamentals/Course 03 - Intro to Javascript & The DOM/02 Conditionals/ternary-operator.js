9ccbd005861fef21bc8d43467a912e328e014f03
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-09 09:53:55 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/02 Conditionals/ternary-operator.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/02 Conditionals/ternary-operator.js
@@ -1,4 +1,12 @@
+// Example 1
 const isGoing = true
 
 const color = isGoing ? "green": "red"
 console.log(color)
+
+// Example 2
+const x = 4;
+const y = 3;
+
+const result = (x + y) > 6 ? 2 * x : 2 * y;
+console.log(result)
