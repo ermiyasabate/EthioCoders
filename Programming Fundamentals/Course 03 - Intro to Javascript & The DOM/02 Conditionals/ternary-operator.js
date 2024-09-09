50c3dd99401e42ee1ff5d393400e57e02d7579be
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-09 09:59:31 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/02 Conditionals/ternary-operator.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/02 Conditionals/ternary-operator.js
@@ -16,3 +16,6 @@ const adult = true;
 const preorder = true;
 
 console.log("It costs $" + (adult ? "40.00" : "20.00") + " to attend the concert. Pick up your tickets at the " + (preorder ? "will call" : "gate") + ".");
+
+console.log("false" ? true: false)
+console.log(false ? "false": "true")
