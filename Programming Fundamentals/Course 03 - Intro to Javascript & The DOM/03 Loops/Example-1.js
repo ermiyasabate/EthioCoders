675a54ec845d9dbdb9ecdbc67ff16d1a661084a3
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-09 18:30:33 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/03 Loops/Example-1.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/03 Loops/Example-1.js
@@ -1,6 +1,15 @@
-let x = 1
-console.log(x + " mississippi")
-x = x + 1
-console.log(x + " mississippi")
-x = x + 1
-console.log(x + " mississippi")
+// let x = 1
+// console.log(x + " mississippi")
+// x = x + 1
+// console.log(x + " mississippi")
+// x = x + 1
+// console.log(x + " mississippi")
+
+
+// Instead of the above one
+let x = 1;
+
+while (x <= 20) {
+    console.log(x + " mississippi!"); 
+    x = x + 1; 
+}
