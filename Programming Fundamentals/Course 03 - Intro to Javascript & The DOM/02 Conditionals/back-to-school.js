33abf7d00a972c60226de3103acd4e9a44c48007
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-09 13:19:26 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/02 Conditionals/back-to-school.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/02 Conditionals/back-to-school.js
@@ -13,10 +13,33 @@
  */
  
 // change the value of `education` to test your code
-const education = "no high school diploma";
+const education = "an Associate's degree";
 
 // set the value of this based on a person's education
 let salary = 0;
 
 // your code goes here
-console.log('oi')
+switch(education) {
+    case "no high school diploma":
+        salary = 25636
+        break
+    case "a high school diploma":
+        salary = 35256
+        break
+    case "an Associate's degree":
+        salary = 41496
+        break
+    case "a Bachelor's degree":
+        salary = 59124
+        break
+    case "a Master's degree":
+        salary = 69732
+        break
+    case "a Professional degree":
+        salary = 89960
+        break
+    case "a Doctoral degree":
+        salary = 84396
+        break
+}
+console.log("In 2015, a person with " + education + " earned an average of $" + salary.toLocaleString() + "/year.")
