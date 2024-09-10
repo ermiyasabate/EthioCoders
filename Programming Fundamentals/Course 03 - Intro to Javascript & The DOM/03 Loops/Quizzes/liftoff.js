cfbe9d911e77b89d22caa0940dc79e737300b6d0
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-10 16:52:30 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/03 Loops/Quizzes/liftoff.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/03 Loops/Quizzes/liftoff.js
@@ -12,3 +12,24 @@
  */
 
 // your code goes here
+let nasaCountdown  = 60
+
+while(nasaCountdown >= 0) {
+    if (nasaCountdown === 50) {
+        console.log("Orbiter transfers from ground to internal power")
+    } else if (nasaCountdown === 31) {
+        console.log("Ground launch sequencer is go for auto sequence start")
+    } else if (nasaCountdown === 16) {
+        console.log("Activate launch pad sound suppression system")
+    } else if (nasaCountdown === 10) {
+        console.log("Activate main engine hydrogen burnoff system")
+    } else if (nasaCountdown === 6) {
+        console.log("Main engine start")
+    } else if (nasaCountdown === 0) {
+        console.log("Solid rocket booster ignition and liftoff!")
+    } else {
+        console.log("T-" + nasaCountdown + " seconds")
+    }
+
+    nasaCountdown = nasaCountdown - 1
+}
