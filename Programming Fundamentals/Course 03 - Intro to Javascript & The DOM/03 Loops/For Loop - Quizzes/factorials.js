5a293c5121d7abaa012dbe8cb221adecc5a85e2e
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-10 17:31:27 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/03 Loops/For Loop - Quizzes/factorials.js
@@ -0,0 +1,10 @@
+/*
+ * Programming Quiz: Factorials
+ */
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should use a `for` loop
+ * - Your code should produce the expected output
+ */
+
+// your code goes here
