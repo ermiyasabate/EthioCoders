71793d36bd364caa39a2e989d37ab74a9ee63b77
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-10 16:42:43 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/03 Loops/Quizzes/liftoff.js
@@ -0,0 +1,14 @@
+/*
+ * Programming Quiz: Countdown, Liftoff!
+ * 
+ * Using a while loop, print out the countdown output above.
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should include a `while` loop
+ * - Your `while` loop should have a stop condition
+ * - Your code should produce the expected output
+ */
+
+// your code goes here
