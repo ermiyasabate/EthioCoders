b6c6df55c0570b7aac480719814b4480590769f1
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-10 17:27:50 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/03 Loops/For Loop - Quizzes/fix1.js
@@ -0,0 +1,17 @@
+/*
+ * Programming Quiz: Fix the Error 1
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should use a `for` loop
+ * - Your `for` loop should specify a starting condition `x` with the value of `5`
+ * - Your `for` loop should have a stop condition for the value of `x`
+ * - Your `for` loop should increment `x` each time it executes
+ * - Your code should produce the expected output
+ */
+ 
+// fix the for loop
+for (x < 10; x++) {
+    console.log(x);
+}
