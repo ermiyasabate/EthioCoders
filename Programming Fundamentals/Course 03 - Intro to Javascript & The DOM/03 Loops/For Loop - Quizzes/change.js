e9a0e1b71b621878003b0bd6adbebcdcc405dfb5
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-10 17:26:18 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/03 Loops/For Loop - Quizzes/change.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/03 Loops/For Loop - Quizzes/change.js
@@ -11,8 +11,8 @@
  */
  
 // rewrite the while loop as a for loop
-let x = 9;
-while (x >= 1) {
-    console.log("hello " + x);
-    x = x - 1;
-}
+// let x = 9;
+// while (x >= 1) {
+//     console.log("hello " + x);
+//     x = x - 1;
+// }
