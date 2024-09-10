27a0a714b206785176cffce6a385d5740ba6ca8e
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-10 14:32:23 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/03 Loops/Quizzes/julia-james.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/03 Loops/Quizzes/julia-james.js
@@ -13,8 +13,16 @@
  
 let x = 1;
 
-while (/* your stop condition goes here */) {
-    // check divisibility
-    // print Julia, James, or JuliaJames
-    // increment x
+while (x <= 20) {
+    if (x % 3 === 0 && x % 5 === 0) {
+        console.log("FizzBuzz")
+    } else if (x % 3 === 0) {
+        console.log("Fizz")
+    } else if (x % 5 === 0) {
+        console.log("Buzz")
+    } else {
+        console.log(x)
+    }
+
+    x = x + 1
 }
