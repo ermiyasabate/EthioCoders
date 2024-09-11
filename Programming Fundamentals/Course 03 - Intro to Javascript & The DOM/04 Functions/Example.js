93d00b64ae31cf8b2bd62c22d8626cb22b533f15
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-11 14:07:50 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM - Function

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/04 Functions/Example.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/04 Functions/Example.js
@@ -1,3 +1,11 @@
+let newWord = ""
+
 function reverseStrings(reverseMe) {
-    
+    for (let i = reverseMe.length; i >= 0; i--) {
+        newWord += reverseMe[i]
+    }
+
+    return newWord;
 }
+
+console.log(reverseStrings("Ermiyas"))
