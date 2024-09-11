7e79c593a79270cdcea42644aead93d39c730582
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-11 16:35:39 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM - Function

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/04 Functions/Quizzes/laugh2.js
@@ -0,0 +1,19 @@
+/*
+ * Programming Quiz: Laugh it Off 2
+ *
+ * QUIZ REQUIREMENTS
+ * - Your code should have a `laugh()` function
+ * - Your `laugh()` function should have one parameter named `num`
+ * - Your `laugh()` function should return a string with `num` number of `"ha"`s.
+ * - The string should end with an exclamation point.
+ */
+
+// your code goes here
+
+
+
+// test code
+console.log(laugh(0)) 
+console.log(laugh(3)) 
+console.log(laugh(4)) 
+console.log(laugh(8)) 
