fc9a83a04950beabe084cc2b5d847ead9ad5d8d0
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-11 16:32:58 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM - Function

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/04 Functions/Quizzes/laugh1.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/04 Functions/Quizzes/laugh1.js
@@ -1,5 +1,16 @@
-function laugh() {
-    return "hahahahahahahahahaha!"
-}
+/*
+ * Programming Quiz: Laugh it Off 1
+ */
 
-console.log(laugh())
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a `laugh()` function
+ * - Your `laugh()` function should return the correct output
+ * - Your code should print `\"hahahahahahahahahaha!\"` by calling the `laugh()`
+ */
+
+// your code goes here
+
+
+// test your code by logging out the returned value
+console.log(/* call the laugh function */);
