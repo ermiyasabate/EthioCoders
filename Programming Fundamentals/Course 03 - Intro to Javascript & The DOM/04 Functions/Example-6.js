4969789f504bc19370589124b30ca3516839ac25
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-12 12:40:48 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM - Function

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/04 Functions/Example-6.js
@@ -0,0 +1,11 @@
+const bookSeeker = "James";
+const book = "Great Expectations";
+function library() {
+    const librarian = "Julia";
+    console.log(bookSeeker + " asked " + librarian + " for " + book);
+    function classicLiterature() {
+        const shelf = "Dickens";
+        console.log(bookSeeker + " found " + book + " on the " + shelf + " shelf!");
+    }
+    classicLiterature();
+}
