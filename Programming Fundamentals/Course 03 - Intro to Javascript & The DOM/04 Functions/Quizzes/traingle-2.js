db682de191524798ac98a67e6a8c8f8e409ffaa2
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-12 18:23:31 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM - Function

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/04 Functions/Quizzes/traingle-2.js
@@ -0,0 +1,31 @@
+/*
+ * Programming Quiz: Build A Triangle (5-3)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a `buildTriangle()` function
+ * - Your `buildTriangle()` function should take one argument (or you can say parameter)
+ * - Your `laugh()` function should build the triangle as described above
+ */
+
+
+// creates a line of * for a given length
+function makeLine(length) {
+    let line = "";
+    for (let j = 1; j <= length; j++) {
+        line += "* ";
+    }
+    return line + "\n";
+}
+
+// your code goes here.  Make sure you call makeLine() in your own code.
+function buildTriangle(length) {
+    for (let i = 0; i <= length; i++) {
+        console.log(makeLine(i));
+    }
+}
+
+// test your code 
+console.log(buildTriangle(10));
+// console.log(makeLine(3));
