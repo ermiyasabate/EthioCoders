e1e84b4f69ddec479b07833e85c989a0ad6c0732
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-12 13:34:34 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM - Function

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/04 Functions/Example-9.js
@@ -0,0 +1,9 @@
+const catSays = function (max) {
+    let catMessage = "";
+    for (let i = 0; i < max; i++) {
+        catMessage += "meow ";
+    }
+    return catMessage;
+};
+
+console.log(catSays)
