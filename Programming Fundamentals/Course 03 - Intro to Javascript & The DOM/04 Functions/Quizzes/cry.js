f25f700b1c94cb4fcbade87a9c446737202887bb
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-15 11:19:20 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM - Function

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/04 Functions/Quizzes/cry.js
@@ -0,0 +1,18 @@
+/*
+ * Programming Quiz: Cry
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `cry`
+ * - Your code should include a named function expression stored in the variable `cry`
+ * - Your code should call the function expression using the variable name, not the function name
+ * - Your function expression should return the expected output
+ */
+
+
+// your code goes here
+
+
+// test your solution
+console.log(cry());
