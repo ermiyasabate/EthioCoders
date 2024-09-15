696b8045a791a93d5de1631cdcf951e98bfc82c7
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-15 14:03:14 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM - Function

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Example-3.js
@@ -0,0 +1,7 @@
+// We can change the value of the an array
+
+const donuts = ["glazed", "chocolate frosted", "Boston cream", "powdered", "sprinkled", "maple", "coconut", "jelly"];
+
+donuts[0] = "Abebe"
+
+console.log(donuts)
