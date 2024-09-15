94fd18fa75d7274596b45866f172a95e32b89a58
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-15 14:38:45 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM - Function

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Quizzes/prices.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: The Price is Right (6-3)
+ */
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `prices`
+ * - The value of the 1st, 3rd, and 7th elements should be doubled
+ * - Your code should print `prices` to the console as an array. Do not iterate over the elements. 
+ */
+ 
+const prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+// your code goes here
