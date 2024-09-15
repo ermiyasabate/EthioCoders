ac04f7f475549ef628304a73fd7adb9e57559128
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-15 12:02:48 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM - Function

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Example-1.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Example-1.js
@@ -1,16 +1,38 @@
-const donut1 = "glazed";
-const donut2 = "chocolate frosted";
-const donut3 = "cinnamon";
-const donut4 = "sprinkled";
-const donut5 = "powdered";
-const donut6 = "cinnamon sugar";
-const donut7 = "glazed cruller";
-const donut8 = "chocolate cruller";
-const donut9 = "cookies";
-const donut10 = "Boston creme";
-const donut11 = "powdered jelly filled";
-const donut12 = "creme de leche";
-const donut13 = "glazed donut holes";
-const donut14 = "blueberry donut holes";
-const donut15 = "cake donut holes";
-const donut16 = "chocolate donut holes"; 
+// From this 
+// const donut1 = "glazed";
+// const donut2 = "chocolate frosted";
+// const donut3 = "cinnamon";
+// const donut4 = "sprinkled";
+// const donut5 = "powdered";
+// const donut6 = "cinnamon sugar";
+// const donut7 = "glazed cruller";
+// const donut8 = "chocolate cruller";
+// const donut9 = "cookies";
+// const donut10 = "Boston creme";
+// const donut11 = "powdered jelly filled";
+// const donut12 = "creme de leche";
+// const donut13 = "glazed donut holes";
+// const donut14 = "blueberry donut holes";
+// const donut15 = "cake donut holes";
+// const donut16 = "chocolate donut holes";
+
+
+// To this
+const donut = [
+    "glazed",
+    "chocolate frosted",
+    "cinnamon",
+    "sprinkled",
+    "powdered",
+    "cinnamon sugar",
+    "glazed cruller",
+    "chocolate cruller",
+    "cookies",
+    "Boston creme",
+    "powdered jelly filled",
+    "creme de leche",
+    "glazed donut holes",
+    "blueberry donut holes",
+    "cake donut holes",
+    "chocolate donut holes"
+]
