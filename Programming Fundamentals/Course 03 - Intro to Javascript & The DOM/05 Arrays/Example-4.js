e993cadea9bd7d0d44f819b2cac9fbbcc14540ab
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-15 14:05:42 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM - Function

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Example-4.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Example-4.js
@@ -3,3 +3,8 @@ const donuts = ["glazed", "chocolate frosted", "boston cream", "powdered", "spri
 donuts[2] = "cinnamon twist";
 donuts[4] = "salted caramel";
 donuts[5] = "shortcake eclair";
+
+// We’ve decided to replace some of donuts in the donuts array.
+// What does the donuts array look like after the following changes?
+
+console.log(donuts)
