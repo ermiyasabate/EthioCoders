fbbc7a20191d2624d033331104b32f0e0991d857
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-15 11:40:11 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM - Function

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/04 Functions/Quizzes/inline.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/04 Functions/Quizzes/inline.js
@@ -13,6 +13,19 @@
 
 // don't change this code
 // emotions() function definition
+let finalWord = ""
+
+const myFunc = function (num) {
+    if (num === 0) {
+        return ""
+    } else {
+        for (let i = 0; i < num; i++) {
+            finalWord += "ha"
+        }
+        return finalWord + "!";
+    }
+}
+
 function emotions(myString, myFunc) {
     console.log("I am " + myString + ", " + myFunc(2));
 }
@@ -21,3 +34,4 @@ function emotions(myString, myFunc) {
 // Call the emotions() function with two arguments
 // Argument 1 - "happy" string
 // Argument 2 - an inline function expression
+emotions("happy", myFunc(2))
