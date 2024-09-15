cea7fe2228fcdd7fdce01be73fb118983168c67a
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-15 14:44:54 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM - Function

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Quizzes/prices.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Quizzes/prices.js
@@ -7,7 +7,7 @@
  * - The value of the 1st, 3rd, and 7th elements should be doubled
  * - Your code should print `prices` to the console as an array. Do not iterate over the elements. 
  */
- 
+
 const prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
 
 // your code goes here
@@ -16,3 +16,22 @@ prices[2] = prices[2] * 2
 prices[6] = prices[6] * 2
 
 console.log(prices)
+
+// OR
+const prices2 = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+for (let index = 0; index < prices2.length; index++) {
+    //change the value of first element
+    if (index === 0) {
+        prices2[index] = prices2[index] * 2;
+    }
+    //change the value of third element
+    else if (index === 2) {
+        prices2[index] = prices2[index] * 2;
+    }
+    //change the value of seventh element
+    else if (index === 6) {
+        prices2[index] = prices2[index] * 2;
+    }
+}
+console.log(prices2);
