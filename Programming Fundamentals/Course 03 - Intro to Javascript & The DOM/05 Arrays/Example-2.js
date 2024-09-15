28a0d15a7ca7465a2b804d3b6a31b7d46ec693f6
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-15 14:00:40 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM - Function

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Example-2.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Example-2.js
@@ -1,3 +1,5 @@
 // What line of code would you use to select the "coconut" donut from the donuts array?
 
 const donuts = ["glazed", "chocolate frosted", "Boston cream", "powdered", "sprinkled", "maple", "coconut", "jelly"];
+
+console.log(donuts[6])
