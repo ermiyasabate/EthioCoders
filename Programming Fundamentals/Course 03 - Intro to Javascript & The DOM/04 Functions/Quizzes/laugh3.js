533bb8c8cd76739fba1a7ff562d9b36e3be861e3
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-15 11:15:00 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM - Function

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/04 Functions/Quizzes/laugh3.js
@@ -0,0 +1,20 @@
+/*
+ * Programming Quiz: Laugh
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `laugh`
+ * - Your code should include an anonymous function expression stored in the variable `laugh`
+ * - Your anonymous function expression should take one argument
+ * - Your anonymous function expression should return the correct number of `hahaha`\'s
+ */
+
+// your code goes here
+const laugh = /* finish the function expression */
+
+
+// testing your code
+console.log(laugh(1));
+console.log(laugh(5));
+console.log(laugh(10));
