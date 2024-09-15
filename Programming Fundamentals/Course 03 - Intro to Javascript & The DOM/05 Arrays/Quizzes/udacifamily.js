81323f40da8b8f2df801de1633a2d82ff41903a7
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-15 14:12:13 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM - Function

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Quizzes/udacifamily.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: UdaciFamily (6-1)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `udaciFamily`
+ * - The variable `udaciFamily` should be an array containing the values `"Julia"`, `"James"`, and one other name
+ * - Your code should print `udaciFamily` to the console
+ */
+
+
+// your code goes here
