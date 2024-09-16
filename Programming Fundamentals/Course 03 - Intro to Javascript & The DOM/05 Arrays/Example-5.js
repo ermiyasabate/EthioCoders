15bee13cb73f45858a53aa219f797861d407f2f4
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-16 21:49:40 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM - Function

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Example-5.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Example-5.js
@@ -1,2 +1,10 @@
 const donuts = ["glazed", "powdered", "sprinkled"];
 console.log(donuts.length);
+
+const inventory = [
+    ["gold pieces", 25],
+    ["belt", 4],
+    ["ring", 1],
+    ["shoes", 2]
+];
+console.log(inventory.length);
