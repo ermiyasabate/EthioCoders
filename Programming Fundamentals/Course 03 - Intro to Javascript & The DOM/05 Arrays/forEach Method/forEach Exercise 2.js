519e4748432bd8484df54f6ccc721cd7350bc563
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-19 17:00:59 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom - Arrays

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/forEach Method/forEach Exercise 2.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/forEach Method/forEach Exercise 2.js
@@ -1,7 +1,14 @@
 // Multiply Each Element by 10: Modify the original array by multiplying each element by 10:
 const numbers = [65, 44, 12, 4];
 
-numbers.forEach(function(array){
-    array = array * 10
-    console.log(array);
+// numbers.forEach(function(array){
+//     array = array * 10
+//     console.log(array);
+// })
+// OR
+
+numbers.forEach(function(array, arrayItem) {
+    array[arrayItem] *= 10
 })
+
+console.log(numbers);
