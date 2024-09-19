acd24adb96189016a99a928d6f52592c0e4ee08b
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-19 14:46:01 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom - Arrays

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Quizzes/rainbow.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: Colors of the Rainbow (6-4)
+ *
+ * Use only the splice() method to modify the rainbow variable:
+ *  - remove "Blackberry"
+ *  - add "Yellow" and "Green"
+ *  - add "Purple"
+ */
+
+const rainbow = ['Red', 'Orange', 'Blackberry', 'Blue'];
+
+// your code goes here
