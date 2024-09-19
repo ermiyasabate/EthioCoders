66786ab4f4212ed2de67324c0f7cb44bdee7f1f9
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-19 16:38:32 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom - Arrays

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/forEach Method.js
@@ -0,0 +1,9 @@
+const donuts = ["jelly donut", "chocolate donut", "glazed donut"];
+
+function myAwesomeFunction(array) {
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i])
+    }
+}
+
+myAwesomeFunction(donuts)
