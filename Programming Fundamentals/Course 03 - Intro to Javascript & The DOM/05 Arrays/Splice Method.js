77d308cfdf01c082f038259bbd38ca25f23b728e
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-19 14:39:16 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom - Arrays

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Splice Method.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Splice Method.js
@@ -3,10 +3,13 @@ donuts.splice(1) // This removes elements from index 1 to the last
 console.log(donuts)
 
 const donutsOne = ["glazed", "chocolate frosted", "Boston creme", "glazed cruller"];
-donutsOne.splice(1, 2) // This removes elements index 1 and the next two elements
+donutsOne.splice(1, 2) // This removes elements index 1 and the next elements
 console.log(donutsOne)
 
 const donutsTwo = ["glazed", "chocolate frosted", "Boston creme", "glazed cruller"];
 donutsTwo.splice(1, 2, "chocolate cruller", "creme de leche") // This removes elements index 1 and the next two elements and adds two donuts
 console.log(donutsTwo)
 
+const donutsThree = ["glazed", "chocolate frosted", "Boston creme", "glazed cruller"];
+donutsThree.splice(-2, 2) // This removes elements the second last element and the element before it
+console.log(donutsThree)
