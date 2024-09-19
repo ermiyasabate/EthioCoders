b5c6979d14005bf1af7793e6fe27bb648cedc1d9
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-19 16:54:48 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom - Arrays

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/forEach Method/forEach Exercise 1.js
@@ -0,0 +1,8 @@
+// Sum of Array Elements: Given an array of numbers, compute their sum using forEach():
+
+const numbers = [65, 44, 12, 4];
+let sum = 0;
+
+numbers.forEach( function(array) {
+    sum += array;
+});
