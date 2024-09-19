097fe2bd968ab49a9fe39ce1b6395628c3af66f0
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-19 15:11:55 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom - Arrays

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Shift Method.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Shift Method.js
@@ -1,1 +1,8 @@
 const removeFirstElement = ["a", "b", "c"];
+const firstElement = removeFirstElement.shift()
+
+console.log(firstElement)
+// Expected output: "a"
+
+console.log(removeFirstElement)
+// Expected output: ["b", "c"]
