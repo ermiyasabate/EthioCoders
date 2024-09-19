d1642e4bcc78686b0e73599c99b59fc633b47d22
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-19 13:37:44 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom - Arrays

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Push Method.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Push Method.js
@@ -1,5 +1,7 @@
 const donuts = ["glazed", "chocolate frosted", "Boston creme", "glazed cruller", "cinnamon sugar", "sprinkled"];
 console.log(donuts)
+console.log(donuts.length)
 
 donuts.push("powdered")
 console.log(donuts)
+console.log(donuts.length)
