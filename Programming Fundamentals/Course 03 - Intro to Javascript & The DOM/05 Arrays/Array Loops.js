3af6af5b76b1c46d28621629548f962ce3b21ba4
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-19 16:25:44 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom - Arrays

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Array Loops.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Array Loops.js
@@ -1,7 +1,8 @@
 const donuts = ["jelly donut", "chocolate donut", "glazed donut"];
 
 for (let i = 0; i < donuts.length; i++) {
-    donuts[i] += " hole"
+    donuts[i] += " hole";
+    donuts[i] = donuts[i].toUpperCase();
 }
 
 console.log(donuts)
