195f49b1755ea6f040e7da9a264049c2eb74597b
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-19 16:49:31 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom - Arrays

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/forEach Method Example.js
@@ -0,0 +1,7 @@
+const donuts = ["jelly donut", "chocolate donut", "glazed donut"];
+
+donuts.forEach(function (array) {
+    array += " hole";
+    array = array.toUpperCase()
+    console.log(array);
+})
