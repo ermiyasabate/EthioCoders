d8f6cd578177e3775fcb11762234d11ab225c9d9
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-22 16:37:28 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM - Array

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/map Method/Example-1.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/map Method/Example-1.js
@@ -1,4 +1,8 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const squaredNumber = numbers.map(squared)
+
+console.log(numbers.toString());
+console.log("Squared: " + squaredNumber);
 
 function squared(element) {
     return Math.pow(element, 2)
