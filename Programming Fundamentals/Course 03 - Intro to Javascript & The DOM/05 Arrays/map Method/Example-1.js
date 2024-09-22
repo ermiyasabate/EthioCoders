cdcbc13604948f5fdef1b1d5f81acd321f3decc1
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-22 16:39:02 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM - Array

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/map Method/Example-1.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/map Method/Example-1.js
@@ -1,5 +1,6 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 const squaredNumber = numbers.map(squared)
+const cubeNumber = numbers.map(cube)
 
 console.log(numbers.toString());
 console.log("Squared: " + squaredNumber);
@@ -7,3 +8,9 @@ console.log("Squared: " + squaredNumber);
 function squared(element) {
     return Math.pow(element, 2)
 }
+
+function cube(element) {
+    return Math.pow(element, 3)
+}
+
+console.log("Cube: " + cubeNumber);
