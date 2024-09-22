ebeba58cdae3cb516e8f839e664c7e1c85c28478
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-22 16:43:37 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM - Array

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/map Method/Example-2.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/map Method/Example-2.js
@@ -1,2 +1,9 @@
 const names = ["Abebe", "Molla", "Moges", "Chala"]
+const nameUppercase = names.map(uppercase)
 
+console.log(names.toString());
+console.log("Uppercase: " + nameUppercase);
+
+function uppercase (element) {
+    return element.toUpperCase()
+}
