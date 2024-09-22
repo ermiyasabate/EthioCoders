e14ffcb71d859a28dedb267259da2568dc1b182b
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-22 16:53:48 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM - Array

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/map Method/Example-3.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/map Method/Example-3.js
@@ -1,6 +1,11 @@
 const dates = ["2024-1-12", "2023-1-12", "2022-1-12"]
+const format = dates.map(formatDates)
+
+console.log("Before: " + dates);
+console.log("Formated: " + format);
+
 
 function formatDates(element) {
-    const newPart = dates.split("-")
+    const newPart = element.split("-")
     return newPart[1] + "/" + newPart[2] + "/" + newPart[0]
 }
