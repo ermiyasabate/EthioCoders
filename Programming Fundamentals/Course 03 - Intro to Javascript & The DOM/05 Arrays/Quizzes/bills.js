597286a6df86843fe25b2c408c75fb264c5d1c2e
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-22 17:03:15 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM - Array

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Quizzes/bills.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/05 Arrays/Quizzes/bills.js
@@ -27,3 +27,11 @@
     100.11, 12.15, 9.90, 29.11, 12.99,
     10.00, 99.22, 102.20, 100.10, 6.77, 2.22
 ];
+const total = bills.map(withTip)
+
+console.log(total);
+
+
+function withTip(element) {
+    return element * 0.15
+}
