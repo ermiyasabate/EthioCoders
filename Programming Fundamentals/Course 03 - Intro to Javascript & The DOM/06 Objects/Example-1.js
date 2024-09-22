2567c3fbd2f1e7a7fbd5d1ff7c14df52cf9865a0
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-22 17:52:13 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to JavaScript and The DOM - Objects

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/06 Objects/Example-1.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/06 Objects/Example-1.js
@@ -1,4 +1,15 @@
 const umbrella = {
     color: "pink",
-    isOpen: false
+    isOpen: false,
+    open: function () {
+        if (this.isOpen) {
+            return "The umbrella is already opened!"
+        } else {
+            umbrella.isOpen = true
+            return "Julia opens the umbrella"
+        }
+    }
 }
+
+console.log(umbrella.open());
+console.log(umbrella.color);
