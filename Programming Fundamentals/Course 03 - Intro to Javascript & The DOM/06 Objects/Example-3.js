dc38779488b564060683b7331fb9587f44a9ddcc
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-24 17:41:55 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom - Objects

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/06 Objects/Example-3.js
@@ -0,0 +1,17 @@
+const savingsAccount = {
+    balance: 1000,
+    interestRatePercent: 1,
+    deposit: function addMoney(amount) {
+        if (amount > 0) {
+            savingsAccount.balance += amount;
+        }
+    },
+    withdraw: function removeMoney(amount) {
+        var verifyBalance = savingsAccount.balance - amount;
+        if (amount > 0 && verifyBalance >= 0) {
+            savingsAccount.balance -= amount;
+        }
+    }
+};
+
+// console.log(savingsAccount.balance);
