7dba83482e26276f2e4457c269dd546c8139ed9f
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-24 17:32:49 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom - Objects

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/06 Objects/Quizzes/menu.js
@@ -0,0 +1,11 @@
+/*
+ * Programming Quiz: Menu Items 
+ */
+
+/*
+ * Create an object named `breakfast`. 
+ * The object should contain properties for the `name`, `price`, and `ingredients`.
+ * Print the entire object on the console
+ */
+
+// your code goes here
