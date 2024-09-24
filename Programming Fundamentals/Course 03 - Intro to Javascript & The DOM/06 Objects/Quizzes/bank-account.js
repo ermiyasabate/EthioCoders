3753c8be410da2340e0196de7d63847a7c48b690
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-24 17:36:18 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom - Objects

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/06 Objects/Quizzes/bank-account.js
@@ -0,0 +1,32 @@
+/*
+ * Programming Quiz: Bank Accounts 1
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have an object `savingsAccount` 
+ * - Your `savingsAccount` object should have the `balance` and `interestRatePercent` property
+ * - Your `savingsAccount` object should have a `printAccountSummary()` method
+ * - Your `printAccountSummary()` method should return the EXACT expected message.
+  * - Be careful about the punctuation, spacing, and spelling of the printed message.
+ */
+
+
+const savingsAccount = {
+    balance: 1000,
+    interestRatePercent: 1,
+    deposit: function addMoney(amount) {
+        if (amount > 0) {
+            savingsAccount.balance += amount;
+        }
+    },
+    withdraw: function removeMoney(amount) {
+        var verifyBalance = savingsAccount.balance - amount;
+        if (amount > 0 && verifyBalance >= 0) {
+            savingsAccount.balance -= amount;
+        }
+    },
+    // your code goes here
+};
+
+console.log(savingsAccount.printAccountSummary());
