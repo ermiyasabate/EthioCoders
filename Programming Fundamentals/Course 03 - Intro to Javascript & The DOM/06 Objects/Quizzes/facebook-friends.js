beebb367870b193079d1f5e655d5c934569ecd17
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-09-24 17:50:00 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom - Objects

---
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/06 Objects/Quizzes/facebook-friends.js
@@ -0,0 +1,33 @@
+/*
+ * Programming Quiz: Facebook Friends
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have an object `facebookProfile`
+ * - The `facebookProfile` object should have the `name` (string), `friends` (number), and `messages` (array of strings) property
+ * - Your `facebookProfile` object should have the `postMessage()`, `deleteMessage()`, `addFriend()` and `removeFriend()` method
+ * - Carefully implement the desired functionality of each method, as decribed above
+ */
+
+
+// your code goes here
+const facebookProfile = {
+    name: "Udacian",
+    friends: 25,
+    messages: ["Message 1", "Message 2", "Message 3", "Message 4"]
+}
+
+
+// text code
+console.log("Name: ", facebookProfile.name);
+console.log("Messages: ", facebookProfile.messages);
+facebookProfile.postMessage("New message!");
+console.log("Messages: ", facebookProfile.messages);
+facebookProfile.deleteMessage(2);
+console.log("Messages: ", facebookProfile.messages);
+console.log("Friends: ", facebookProfile.friends);
+facebookProfile.addFriend();
+console.log("Friends: ", facebookProfile.friends);
+facebookProfile.removeFriend();
+console.log("Friends: ", facebookProfile.friends);
