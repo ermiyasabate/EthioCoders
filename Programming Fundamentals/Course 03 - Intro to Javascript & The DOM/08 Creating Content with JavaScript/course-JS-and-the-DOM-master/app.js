874160583adb163ada374db05f04adca5455419a
Ermiyas Abate <ermiyasabateg@gmail.com>
2024-10-10 15:45:33 +0300

    EthioCoders - Programming Fundamentals - Course 03 - Intro to Javascript and The Doom - Creating content with JavaScript

--- Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/08 Creating Content with JavaScript/course-JS-and-the-DOM-master/app.js
+++ Programming Fundamentals/Course 03 - Intro to Javascript & The DOM/08 Creating Content with JavaScript/course-JS-and-the-DOM-master/app.js
@@ -2,3 +2,8 @@ const start = document.querySelector('#start-now');
 
 start.style.backgroundColor = '#2015ff';
 start.style.borderRadius = '5rem';
+
+const hero = document.querySelector('.hero--homepage');
+hero.classList.add('new-hero');
+
+const cardList = document.querySelectorAll('.card');
